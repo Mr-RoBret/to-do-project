5908a0668d9993929b16c6f8d0921d8653c2468b
Mr Robret <bret.farley@gmail.com>
2021-04-22 23:52:34 -0500

    Added list and array functions, added formatting.

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 /**
  * function to add item to list
  */
-const addItem = (newTask, taskList) => {
-    const listLength = taskList.length
-    if (listLength > 0) {
-        let existingList = document.getElementsByClassName("task-item")
-        let li = document.createElement("li")
-        li.appendChild(document.createTextNode(newTask))
-        existingList.appendChild(li)
-    } else {
-        let newList = document.createElement("ol")
-        newList.classList.add("task-item")
-        //add li
-        let li = document.createElement("li")
-        li.appendChild(document.createTextNode(newTask))
-        //append newTask
-        newList.appendChild(li)
-        console.log(newList)
-    }
+const addItem = (item) => {
+    // get existing list from DOM
+    let existingList = document.getElementById("task-list")
 
+    // create li and add class, text
+    let li = document.createElement("li")
+    li.classList.add("task-item")
+    li.appendChild(document.createTextNode(item))
+
+    // create button for controling li
+    let listButton = document.createElement("button")
+    listButton.classList.add("item-ctrl")
+    listButton.textContent = "-"
+
+    //combine li and button into new element
+    let itemElement = document.createElement("div")
+    itemElement.classList.add("item-element")
+    itemElement.appendChild(listButton)
+    itemElement.appendChild(li)
+
+    //add new element to list at bottom of DOM
+    existingList.appendChild(itemElement)
 } 
+
+/**
+ * function to append item to list array
+ */
+const addArrayItem = (newTask, taskList) => {
+    //get current length of array
+    let itemIndex = taskList.length
+    //add newTask to array of tasks
+    taskList.push(newTask)
+    //for each new task in array, create a list item
+    addItem(taskList[itemIndex])
+}
+
 /**
  * function to remove item from list
  */
@@ -29,7 +46,7 @@ const addItem = (newTask, taskList) => {
  */
 const taskList = []
 const taskForm = document.querySelector("form")
-const formEntry = document.getElementById("userInput")
+const formEntry = document.getElementById("user-input")
 const button = document.querySelector("button")
 
 /**
@@ -39,7 +56,7 @@ taskForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log("clicked!")
     let newTask = formEntry.value
-    console.log(newTask)
-    addItem(newTask, taskList)
-    formEntry.textContent = ""
+    console.log(`formEntry textContent is currently: ${newTask}`)
+    addArrayItem(newTask, taskList)
+    formEntry.value = ""
 })
